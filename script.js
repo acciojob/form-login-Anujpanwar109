65e7629767033e95f6dc62135b572223f88ae8f8
Anujpanwar109 <166197689+Anujpanwar109@users.noreply.github.com>
2025-02-19 06:48:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 function getFormvalue() {
     //Write your code here
-
+	
+	let inputF=document.querySelector('[name="fname"]').value;
+	let inputL=document.querySelector('[name="lname"]').value;
+	if(inputF===""||inputL===""){
+		alert("invalid input")
+		return;
+	}else{
+		alert(`${inputF} ${inputL}`)
+	}
 }
